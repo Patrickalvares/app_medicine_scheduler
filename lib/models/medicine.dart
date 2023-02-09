9de9e6c988a7a5db01f3347852693322aa51d905
Notebook Patrick <patrickalvares@outlook.com>
2023-02-08 21:25:57 -0300

    controller creation

--- lib/models/medicine.dart
+++ lib/models/medicine.dart
@@ -1,28 +1,45 @@
-class Medicine {
+abstract class Medicine {
   String name;
   DateTime initialDate;
-  RecurrenceType recurrenceType;
+
   bool active;
-  int? period;
-  List<DateTime>? specificHours;
   String? observation;
 
   Medicine(
     this.name,
-    this.initialDate,
-    this.recurrenceType, {
+    this.initialDate, {
     this.active = true,
-    this.period,
-    this.specificHours,
     this.observation,
   });
 }
 
-enum RecurrenceType {
-  daily,
-  weekly,
-  monthly,
-  daysPeriodic,
-  hoursPeriodic,
-  specificHours,
+class DailyMedicine extends Medicine {
+  DailyMedicine(super.name, super.initialDate);
 }
+
+class WeeklyMedicine extends Medicine {
+  WeeklyMedicine(super.name, super.initialDate);
+}
+
+class MonthlyMedicine extends Medicine {
+  MonthlyMedicine(super.name, super.initialDate);
+}
+
+class PeriodicMedicine extends Medicine {
+  Duration period;
+  PeriodicMedicine(super.name, super.initialDate, this.period);
+}
+
+class SpecificHoursMedicine extends Medicine {
+  List<Duration> offset;
+  SpecificHoursMedicine(super.name, super.initialDate, this.offset);
+}
+
+// enum RecurrenceType {
+//   daily,
+//   weekly,
+//   monthly,
+//   daysPeriodic,
+//   hoursPeriodic,
+//   specificHours,
+// }
--- lib/pages/new_medicine.dart
+++ lib/pages/new_medicine.dart
@@ -1,4 +1,8 @@
+import 'package:app_medicine_scheduler/bloc/medicine_bloc.dart';
+import 'package:app_medicine_scheduler/bloc/medicine_event.dart';
+import 'package:app_medicine_scheduler/models/medicine.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewMedicine extends StatefulWidget {
   const NewMedicine({super.key});
@@ -8,16 +12,20 @@ class NewMedicine extends StatefulWidget {
 }
 
 class _NewMedicineState extends State<NewMedicine> {
-  final dropDownItems = [
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _medicineName = TextEditingController();
+  final TextEditingController _medicineObservation = TextEditingController();
+
+  static const List<String> dropDownItems = [
     ' Diariamente',
     ' Semanalmente',
     ' Mensalmente',
-    ' A cada X dias',
-    ' A cada X horas',
-    ' Horários Especificos',
+    ' A cada __ dias',
+    ' A cada __ horas',
   ];
 
-  String? value;
+  String? recurrenceTypevalue;
+  String? medicineFrequencyType;
 
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
         value: item,
@@ -54,86 +62,131 @@ class _NewMedicineState extends State<NewMedicine> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text(
-                'Nome do Medicamento:',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 8.0, right: 8, top: 3, bottom: 20),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                ),
-                child: TextFormField(
-                  style: const TextStyle(fontSize: 22),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Nome do Medicamento:',
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text('Qual a frequência de uso?',
-                  style: TextStyle(fontSize: 18)),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 8.0, right: 8, top: 3, bottom: 20),
-              child: Container(
-                decoration:
-                    BoxDecoration(border: Border.all(style: BorderStyle.solid)),
-                child: DropdownButton<String>(
-                  underline: const SizedBox(),
-                  items: dropDownItems.map(buildMenuItem).toList(),
-                  onChanged: ((value) => setState(() => this.value = value)),
-                  value: value,
-                  isExpanded: true,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text(
-                'Observação:',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 8.0, right: 8, top: 3, bottom: 20),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                ),
-                child: TextFormField(
-                  style: const TextStyle(fontSize: 22),
-                ),
-              ),
-            ),
-            Center(
-              child: FloatingActionButton.extended(
-                backgroundColor: Colors.black,
-                onPressed: () {},
-                label: Padding(
-                  padding: const EdgeInsets.only(left: 25, right: 25),
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.save,
-                        color: Colors.white,
-                      ),
-                      Text(' Salvar'),
-                    ],
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, right: 8, top: 3, bottom: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: TextFormField(
+                    controller: _medicineName,
+                    style: const TextStyle(fontSize: 22),
                   ),
                 ),
               ),
-            )
-          ],
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text('Qual a frequência de uso?',
+                    style: TextStyle(fontSize: 18)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, right: 8, top: 3, bottom: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(style: BorderStyle.solid)),
+                  child: DropdownButtonFormField<String>(
+                    items: dropDownItems.map(buildMenuItem).toList(),
+                    onChanged: ((value) =>
+                        setState(() => recurrenceTypevalue = value)),
+                    value: recurrenceTypevalue,
+                    isExpanded: true,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Observação:',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 8.0, right: 8, top: 3, bottom: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: TextFormField(
+                    controller: _medicineObservation,
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                ),
+              ),
+              Center(
+                child: FloatingActionButton.extended(
+                  backgroundColor: Colors.black,
+                  onPressed: () {
+                    late Medicine medicine;
+                    switch (recurrenceTypevalue) {
+                      case ' Diariamente':
+                        {
+                          medicine =
+                              DailyMedicine(_medicineName.text, DateTime.now());
+                          break;
+                        }
+                      case ' Semanalmente':
+                        {
+                          medicine = WeeklyMedicine(
+                              _medicineName.text, DateTime.now());
+                          break;
+                        }
+                      case ' Mensalmente':
+                        {
+                          medicine = MonthlyMedicine(
+                              _medicineName.text, DateTime.now());
+                          break;
+                        }
+                      case ' A cada __ dias':
+                        {
+                          medicine = PeriodicMedicine(_medicineName.text,
+                              DateTime.now(), const Duration(days: 3));
+                          break;
+                        }
+                      case ' A cada __ horas':
+                        {
+                          medicine = PeriodicMedicine(_medicineName.text,
+                              DateTime.now(), const Duration(hours: 2));
+                          break;
+                        }
+                      default:
+                        throw Exception(
+                            "Erro na seleção do tipo de Frequência");
+                    }
+                    BlocProvider.of<MedicineBloc>(context)
+                        .add(AddMedicineEvent(medicine));
+                    Navigator.pop(context);
+                  },
+                  label: Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.save,
+                          color: Colors.white,
+                        ),
+                        Text(' Salvar'),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

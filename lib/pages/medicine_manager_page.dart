64c8e8acfa14787b1c00d28e91411c44fee7aac0
Notebook Patrick <patrickalvares@outlook.com>
2023-03-08 11:29:15 -0300

    delete button on medicine manager

--- lib/pages/medicine_manager_page.dart
+++ lib/pages/medicine_manager_page.dart
@@ -22,81 +22,101 @@ class _MedicinesManagerPageState extends State<MedicinesManagerPage> {
         backgroundColor: Colors.transparent,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              'Adicionar Medicamento',
+          children: const [
+            Text(
+              'Gerenciar Medicamento',
               style: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontSize: 23),
             ),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.manage_accounts_rounded,
-                  color: Colors.black,
-                  size: 35,
-                ))
           ],
         ),
       ),
-      body: Expanded(
-        flex: 3,
-        child: BlocBuilder(
-            bloc: BlocProvider.of<MedicineBloc>(context),
-            builder: (context, state) {
-              if (state is MedicineEmptyState) {
-                return const Center(child: Text('Não há Remédios'));
-              } else if (state is MedicineLoadedState) {
-                List<MedicineSchedule> schedules = [];
+      body: BlocBuilder(
+          bloc: BlocProvider.of<MedicineBloc>(context),
+          builder: (context, state) {
+            if (state is MedicineEmptyState) {
+              return const Center(child: Text('Não há Remédios'));
+            } else if (state is MedicineLoadedState) {
+              List<MedicineSchedule> schedules = [];
 
-                for (var medicine in state.medicines) {
-                  if ((medicine is DailyMedicine) ||
-                      (medicine is WeeklyMedicine) ||
-                      (medicine is MonthlyMedicine) ||
-                      (medicine is PeriodicMedicine)) {
-                    DateTime day = DateTime.now();
-                    schedules.add(MedicineSchedule(day, medicine));
-                  }
+              for (var medicine in state.medicines) {
+                if ((medicine is DailyMedicine) ||
+                    (medicine is WeeklyMedicine) ||
+                    (medicine is MonthlyMedicine) ||
+                    (medicine is PeriodicMedicine)) {
+                  DateTime day = DateTime.now();
+                  schedules.add(MedicineSchedule(day, medicine));
                 }
+              }
 
-                return ListView.builder(
-                  itemCount: schedules.length,
-                  itemBuilder: ((context, index) {
-                    MedicineSchedule medicine = schedules[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 5, top: 5, left: 10, right: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(),
-                            borderRadius: BorderRadius.circular(8)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(medicine.medicine.name),
-                                    Text(medicine.medicine.observation
-                                        .toString())
-                                  ]),
-                              Text(
-                                  "${medicine.medicine.periodicKind} ${medicine.medicineTime.hour}:${medicine.medicineTime.minute}"),
-                            ],
-                          ),
+              return ListView.builder(
+                itemCount: schedules.length,
+                itemBuilder: ((context, index) {
+                  MedicineSchedule medicine = schedules[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        bottom: 5, top: 5, left: 10, right: 10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(medicine.medicine.name),
+                                  Text(medicine.medicine.observation.toString())
+                                ]),
+                            Text(
+                                "${medicine.medicine.periodicKind} ${medicine.medicineTime.hour}:${medicine.medicineTime.minute}"),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Confirmação'),
+                                      content: const Text(
+                                          'Tem certeza que deseja deletar?'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(
+                                              context, 'Cancelar'),
+                                          child: const Text('Cancelar'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context, 'Deletar'),
+                                          child: const Text('Deletar'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                ).then((value) {
+                                  if (value == 'Deletar') {
+                                    // Coloque o código para deletar aqui
+                                  }
+                                });
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  }),
-                );
-              } else {
-                return const Text("Erro doido");
-              }
-            }),
-      ),
+                    ),
+                  );
+                }),
+              );
+            } else {
+              return const Text("Erro doido");
+            }
+          }),
     );
   }
 }
